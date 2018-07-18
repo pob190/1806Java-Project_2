24a997376f98c32e53e841a41c78a89b3f8c1532
stevanmcunningham <stevanc08@gmail.com>
2018-07-18 19:37:32 -0400

    login template completed; 2 way-binding for username/pass; NO FUNCTIONALITY

---
+++ RapidQuizAngular/src/app/components/login-component/login-component.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { User } from '../../user';
+
+@Component({
+  selector: 'app-login-component',
+  templateUrl: './login-component.component.html',
+  styleUrls: ['./login-component.component.scss']
+})
+export class LoginComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+  
+  onSubmit(){
+
+  }
+
+}
